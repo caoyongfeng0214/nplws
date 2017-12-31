d7a0059641b74cc85749419ed4f52fd12338c382
CYF <me@caoyongfeng.com>
2017-12-31 18:17:41 +0800

    init

---
+++ npl_mod/ws/connection.lua
@@ -0,0 +1,55 @@
+﻿NPL.load('common');
+local connection = {};
+
+connection._cnns = {};
+
+function connection:new(o)
+    o = o or {};
+    local a = setmetatable(o, { __index = self });
+    connection._cnns[o.nid or o.tid] = a;
+    return a;
+end;
+
+
+function connection:on(evtName, fn)
+    self['on' .. evtName] = fn;
+end
+
+
+function connection:recvMsg(msg)
+    if(self.onmessage) then
+        self:onmessage(msg[1]);
+    end
+end
+
+
+connection.onRecvEventMsg = function(msg)
+    local nid = msg.nid or msg.tid;
+    if(nid) then
+        local cn = connection._cnns[nid];
+        if(cn) then
+            if(msg.code == NPLReturnCode.NPL_ConnectionDisconnected) then
+                if(cn.onclose) then
+                    cn:onclose();
+                end
+            end
+        end
+    end
+end;
+
+
+local function activate()
+    local nid = msg.nid or msg.tid;
+    if(nid) then
+        local cn = connection._cnns[nid];
+        if(cn) then
+            cn:recvMsg(msg);
+        end
+    end
+end
+
+
+NPL.this(activate);
+
+
+return connection;
